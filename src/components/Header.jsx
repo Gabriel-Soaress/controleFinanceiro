4f25efd1c94862e086650d5dc675fae55cfc7130
Gabriel <gabrielpedrosa363@gmail.com>
2026-01-29 11:03:28 -0300

    AJUSTE LINK ROTAS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import styles from "../modules/Header.module.css"
-import logo from "../assets/images/logo.png"
+// import logo from "../assets/images/logo.png"
 
 // 1. Recebemos a prop 'aoSair' aqui nos argumentos
 function Header({ aoSair }) {
     return (
         <div className={styles.Header}>
 
-            <img src={logo} alt="Logo Sofit" />
+            {/*<img src={logo} alt="Logo Sofit" />*/}
 
             {/* 2. No clique, chamamos a função que veio do pai */}
             <button className={styles.botaoSair} onClick={aoSair}>
